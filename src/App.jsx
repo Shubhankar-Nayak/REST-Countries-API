4651d2b4bcb8c34834eb86c8ae9826df7f46949d
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-01-03 03:25:19 +0530

    BrowerRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React ,{ useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Country from "./Country";
 
 function App() {
 
   return (
-    <Router basename='/'>
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/country/:name' element={<Country />} />
